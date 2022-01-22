c5a17a7a741934776424e5bb42604dae558c01d2
chetan1bro <chetan1bro@yahoo.com>
2022-01-23 00:53:53 +0530

    initial commit

---
+++ public/script.js
@@ -0,0 +1,42 @@
+let canvas = document.getElementById('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var io = io.connect('http://localhost:8080/');
+let ctx = canvas.getContext("2d");
+
+let x;
+let y;
+let mouseDown =false;
+
+window.onmousedown = (e) => {
+    mouseDown = true;
+    io.emit('down', ({x,y}));
+    ctx.moveTo(x,y);
+}
+
+window.onmouseup = (e) => {
+     
+    mouseDown = false;
+}
+
+io.on("ondraw",({x,y}) =>{
+    ctx.lineTo(x,y);
+    ctx.stroke();
+
+});
+
+io.on('ondown' , ({x,y})=>{
+    ctx.moveTo(x,y);
+})
+window.onmousemove = (e) => {
+    x = e.clientX;
+    y = e.clientY;
+    
+    if(mouseDown)
+    {
+        io.emit("draw",{x , y});
+    ctx.lineTo(x,y);
+    ctx.stroke();
+    }
+}
